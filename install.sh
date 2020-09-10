ca7fee0094fe6e0d3e0ac521f94537c0dfad59b7
Ente <ducksource@duckpond.ch>
2020-09-10 22:26:55 +0200

    ln in install script

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ORIGIN_URL="$( cd "${DIR}" && git config --get remote.origin.url )"
 MACHINES_DIR="${DIR}/machines"
 NIX_CFG_DIR="/etc/nixos"
 
@@ -10,26 +9,19 @@ MACHINENAME="${1?Machinename not set}"
 
 if [ "$EUID" -ne 0 ]; then
   echo "Must run $0 as root" >&2
-  exit -1
+  exit 1
 fi
 
 if [ ! -d "${MACHINES_DIR}/${MACHINENAME}" ]; then
-  echo "Unmanaged machine: ${MACHINENAME}" >&2 && exit -1
+  echo "Unmanaged machine: ${MACHINENAME}" >&2
+  exit 1
 fi
 
-git clone \
-  --quiet \
-  --recursive \
-  "${DIR}" "${NIX_CFG_DIR}"
-
-git \
-  -C "${NIX_CFG_DIR}" \
-remote set-url \
-  "origin" "${ORIGIN_URL}"
-
-cat >"${NIX_CFG_DIR}/configuration.nix" <<EOF
+cat >"${DIR}/configuration.nix" <<EOF
 import ./machines/${MACHINENAME}
 EOF
+
+ln -s "${DIR}" "${NIX_CFG_DIR}"
 
 echo "Adding nix-channels"
 
