455f65c20575af529ebf4b30113a69fcba080bcb
Ente <ducksource@duckpond.ch>
2020-12-27 20:44:50 +0100

    remove users option

--- modules/keepass-duckpond.nix
+++ modules/keepass-duckpond.nix
@@ -34,7 +34,9 @@ in {
       "x-systemd.automount"
       "x-systemd.device-timeout=30"
       "_netdev"
-      "users"
+      # User option broken, but should work anyways:
+      # https://github.com/karelzak/util-linux/issues/1193
+      #"users"
       "idmap=user"
       "ControlMaster=no"
       "allow_other"
