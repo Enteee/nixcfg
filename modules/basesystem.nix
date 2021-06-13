5cf83d155d9c0eac6a7508e62483c0bf967bc941
Ente <ducksource@duckpond.ch>
2021-06-13 21:10:09 +0200

    switch to pipewire

--- modules/basesystem.nix
+++ modules/basesystem.nix
@@ -78,16 +78,22 @@ in {
   # Autorandr
   services.autorandr.enable = true;
 
-  # Enable sound.
-  sound = {
-    enable = true;
-    mediaKeys.enable = true;
-  };
+  # Remove sound.enable or turn it off if you had it set previously, it seems to cause conflicts with pipewire
+  #sound.enable = false;
 
-  # pulseaudio with bluetooth
-  hardware.pulseaudio = {
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
     enable = true;
-    package = pkgs.pulseaudioFull;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
 
   fonts.fonts = with pkgs; [
