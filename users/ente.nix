7542035d7975d979fef92e80978fec1867cc7526
Ente <ducksource@duckpond.ch>
2022-12-20 20:57:24 +0100

    floating steam

--- users/ente.nix
+++ users/ente.nix
@@ -547,10 +547,22 @@ in {
               command = "move down";
               criteria = { class = "Remote-viewer"; };
             }
-            {
-              command = "floating enable";
-              criteria = { class="Hopper"; instance="hopper"; title="Hopper Disassembler v4"; };
-            }
+          ];
+
+          floating.criteria = [
+              { class="Hopper"; instance="hopper"; title="Hopper Disassembler v4"; }
+
+              # https://github.com/ValveSoftware/steam-for-linux/issues/1040
+              { class="^Steam$"; title="^Friends$"; }
+              { class="^Steam$"; title="Steam - News"; }
+              { class="^Steam$"; title=".* - Chat"; }
+              { class="^Steam$"; title="^Settings$"; }
+              { class="^Steam$"; title=".* - event started"; }
+              { class="^Steam$"; title=".* CD key"; }
+              { class="^Steam$"; title="^Steam - Self Updater$"; }
+              { class="^Steam$"; title="^Screenshot Uploader$"; }
+              { class="^Steam$"; title="^Steam Guard - Computer Authorization Required$"; }
+              { title="^Steam Keyboard$"; }
           ];
 
           keybindings = with config.xsession.windowManager.i3.config; mkOptionDefault {
